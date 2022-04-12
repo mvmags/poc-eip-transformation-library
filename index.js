75f6a89a02eeec10bd11c934f7dcfbaa8af4e2f3
mvmags <marco.velasco@gmail.com>
2022-04-12 13:06:18 -0500

    updated files

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const fs = require('fs')
 const { TransformerFactory, TransformerTypes } = require('eip-transformation-library')
 
 // here I'm loading couple of files that contains a Variant Report xml file and an XSL file to transform it
-const xml = fs.readFileSync('./examples/variant-report-2.2.xml')
-const xsl = fs.readFileSync('./examples/variant-report-2.2-to-external.xsl')
-const xsd = fs.readFileSync('./examples/variant-report-2.2-external.xsd')
-const jsonSchema = fs.readFileSync('./examples/variant-report-2.2-external-schema.json')
+const xml = fs.readFileSync('./examples/variant-report-2.2.xml') // original xml version from compbio
+const xsl = fs.readFileSync('./examples/variant-report-2.2-to-external.xsl') // xsl transformation file
+const xsd = fs.readFileSync('./examples/variant-report-2.2-external.xsd') // xsd schema validation file
+const jsonSchema = fs.readFileSync('./examples/variant-report-2.2-external-schema.json') // json schema validation file
 
 // here I'm getting an instance of the Variant Report transformer by making use of the FActory
 const transformer = TransformerFactory.getInstance(TransformerTypes.variantReportTransformer)
