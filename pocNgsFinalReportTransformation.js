bd4820ae43db91a1b8b6309e3b1abb0cec4342cc
mvmags <marco.velasco@gmail.com>
2022-04-26 21:27:21 -0500

    updated poc

--- pocNgsFinalReportTransformation.js
+++ pocNgsFinalReportTransformation.js
@@ -3,10 +3,9 @@ const fs = require('fs')
 const { TransformerFactory, TransformerTypes } = require('eip-transformation-library')
 
 // here I'm loading couple of files that contains a Variant Report xml file and an XSL file to transform it
-const xml = fs.readFileSync('./examples/variant-report-2.2.xml') // original xml version from compbio
-const xsl = fs.readFileSync('./examples/variant-report-2.2-to-external.xsl') // xsl transformation file
-const xsd = fs.readFileSync('./examples/variant-report-2.2-external.xsd') // xsd schema validation file
-const jsonSchema = fs.readFileSync('./examples/variant-report-2.2-external-schema.json') // json schema validation file
+const xml = fs.readFileSync('./examples/finalReport/finalReport.xml') // original xml version from compbio
+const xsl = fs.readFileSync('./examples/finalReport/ClinicalReport.1.1.xsl') // xsl transformation file
+const jsonSchema = fs.readFileSync('./examples/finalReport/finalReport-schema.json') // json schema validation file
 
 // here I'm getting an instance of the Variant Report transformer by making use of the FActory
 const transformer = TransformerFactory.getInstance(TransformerTypes.ngsFinalReport)
@@ -17,13 +16,13 @@ transformer.transformXml(xml.toString(), xsl.toString())
 
 // here printing the result of the transformation
 if (transformer.result.errors.length === 0) {
-  console.log('--- example XML Variant Report transformed from internal to external FMI------\n\n', transformer.result.content)
-  fs.writeFileSync('./output/transformed.xml', transformer.result.content)
+  console.log('--- example XML NGS Final Report transformed from internal to external FMI------\n\n', transformer.result.content)
+  fs.writeFileSync('./output/finalReportTransformed.xml', transformer.result.content)
 }
 
 // example transforming XML Variant Report and convert to JSON
 transformer.transformXmlAndConvertToJson(xml.toString(), xsl.toString(), jsonSchema.toString())
 if (transformer.result.errors.length === 0) {
-  fs.writeFileSync('./output/transformed.json', JSON.stringify(JSON.parse(transformer.result.content), null, 2))
-  console.log('--- example XML Variant Report transformed from internal to external FMI and converted to JSON format------\n\n', JSON.stringify(JSON.parse(transformer.result.content), null, 2))
+  console.log('--- example XML NGS Final Report transformed from internal to external FMI and converted to JSON format------\n\n', JSON.stringify(JSON.parse(transformer.result.content), null, 2))
+  fs.writeFileSync('./output/finalReportTransformed.json', JSON.stringify(JSON.parse(transformer.result.content), null, 2))
 }
